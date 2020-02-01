9ca5a016a63a7f16240deb0f4d0959775e5ebebe
Tom Draper <tgdrape@gmail.com>
2020-02-01 14:04:46 -0800

    Captures timestamped images every sleep_seconds.

--- capture_test.py
+++ capture_test.py
@@ -1,12 +1,21 @@
-from cv2 import *
+import cv2 as cv
+import datetime
+import time
 # initialize the camera
 
-for i in [0,2,4]:
-    cam = VideoCapture(i)   # 0 -> index of camera
-    s, img = cam.read()
-    if s:    # frame captured without any errors
-        #namedWindow("cam-test",WINDOW_AUTOSIZE)
-        #imshow("cam-test%d"%i,img)
-        #waitKey(0)
-        #destroyWindow("cam-test%d"%i)
-        imwrite("test%d.jpg"%i,img) #save image
+while True:
+    for i in [0,2,4]:
+        cam = cv.VideoCapture(i)   # 0 -> index of camera
+        s, img = cam.read()
+        if s:    # frame captured without any errors
+            # cv.namedWindow("cam-test", cv.WINDOW_AUTOSIZE)
+            # cv.imshow("cam-test%d"%i,img)
+            # cv.waitKey(0)
+            # cv.destroyWindow("cam-test%d"%i)
+            now = datetime.datetime.now()
+            filename = now.isoformat() + '_camera_%d.jpg'%i
+            print('Writing file %s'%filename)
+            cv.imwrite(filename,img) #save image
+    sleep_seconds = 5
+    print('Sleeping for %d seconds.'%sleep_seconds)
+    time.sleep(sleep_seconds)
